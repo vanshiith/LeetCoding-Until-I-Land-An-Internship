b1e6e90df3cb48ea664c5a9197cae3328c1568df
Vansh Bhandari <vanshbhandari@Vanshs-MacBook-Air.local>
2023-07-03 03:07:09 +0530

    Changes to this file

--- removeElement.js
+++ removeElement.js
@@ -28,3 +28,18 @@ var removeDuplicates = function(nums) {
     }
     return count
 };
+
+//Remove duplicates 2
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let count = 0;
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] !== nums[i+1] || (nums[i] === nums[i+1] && nums[i+1] !== nums[i+2])){
+            nums[count++] = nums[i]
+        }
+    }
+    return count
+};
